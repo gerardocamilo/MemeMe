10b7adcd7eda65e4b247073dcb5b8e9c3a64450d
Gerardo Camilo <gerardo.camilo@gmail.com>
2017-04-22 14:02:45 -0400

    Initial commit. Pick image from library, display picked image and cancel selection functionality in place

---
+++ MemeMe/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Gerardo Camilo on 4/22/17.
+//  Copyright © 2017 Gerardo Camilo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func pickImage(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imageView.image = image
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+
+    }
+    
+}
+
